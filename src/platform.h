2565a87de4c02ecb8c2b448fb4b2c8f46a3965f6
Matt Sturgeon <matt@sturgeon.me.uk>
2016-07-04 02:18:16 +0100

    Port to GLFW/OpenGL - builds on linux(!)
     - Replaced DX9 example code with GLFW example code (from imgui)
     - Added GLFW 3.2 submodule
     - Adjusted CMake files
     - Added .travis.yml with support for github deployments
     - Added travis build badge to README
     - Removed embedded assets and LoadAsset function
       - Transitioned icon to GLFW_ICON
     - Ported keyboard bindings to GLFW
     - Ported drag and drop to GLFW
       - Made BoardView app{} a global object to aid access.
     - Ported filename in title to GLFW
     - Tweak the README since we are not using DX9 anymore
     - Added files to the build system to aid cross platform builds
       - Added freedesktop hicolor icon pack
       - Added .desktop file
       - Added mimetype file
       - Added files for OSX bundle (e.g. .plists file)
       - Added files for .deb and .rpm packages (e.g. post-install sctipt)

--- src/platform.h
+++ src/platform.h
@@ -3,9 +3,6 @@
 #include "imgui.h"
 #include "utf8.h"
 
-// Windows .rc definitions
-#include "resource.h"
-
 // Loads an entire file in to memory.  The returned buffer must be later freed by the caller.
 char *file_as_buffer(size_t *buffer_size, const char *utf8_filename);
 
@@ -16,5 +13,3 @@ char *file_as_buffer(size_t *buffer_size, const char *utf8_filename);
 // could be done in a more elegant way in the future, if the scope of the project expands to include
 // lots of formats)
 char *show_file_picker();
-
-unsigned char *LoadAsset(int *asset_size, int asset_id);

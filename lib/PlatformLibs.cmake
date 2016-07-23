725f4ec88bc9e656c938d82743d330332361e13e
Matt Sturgeon <matt@sturgeon.me.uk>
2016-07-23 06:58:32 +0100

    Don't use system GLFW for travis
    Can't find an up to date version of GLFW for ubuntu trusty so don't
    bother.

--- lib/PlatformLibs.cmake
+++ lib/PlatformLibs.cmake
@@ -44,7 +44,7 @@ option(USE_SYSTEM_GLFW
 	"Try to use the system installed GLFW. Will fallback to included version if GLFW can't be found."
 ON)
 if(USE_SYSTEM_GLFW)
-	find_package(GLFW3)
+	find_package(GLFW3 3.2)
 	if(GLFW3_FOUND)
 		message("\n  Using system GLFW\n")
 		set(PLATFORM_LIBS ${PLATFORM_LIBS} ${GLFW3_LIBRARY})

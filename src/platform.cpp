8f7133c1748a25d9a78edf6b45c532c4ff23ef1d
Matt Sturgeon <matt@sturgeon.me.uk>
2016-07-27 14:13:57 +0100

    Add file_write_lines() and buffer_to_lines()
     * write_lines() (currently unused) writes lines from an array into a
       file. It can optionally append instead of overriting.
    
     * buffer_to_lines() takes a char * buffer and creates an array of strings
       representing each line.
    
    Primarily intended for use with the history object and potentially writing
    to a config file. buffer_to_lines() may be able to replace stringfile() in
    BRDFile.cpp

--- src/platform.cpp
+++ src/platform.cpp
@@ -4,6 +4,7 @@
 #include <assert.h>
 #include <nowide/fstream.hpp>
 #include <string>
+#include <vector>
 
 char *file_as_buffer(size_t *buffer_size, const char *utf8_filename) {
 	return file_as_buffer(buffer_size, std::string(utf8_filename));
@@ -27,4 +28,67 @@ char *file_as_buffer(size_t *buffer_size, const std::string &utf8_filename) {
 	file.close();
 
 	return buf;
+}
+
+std::vector<std::string> buffer_to_lines(char *buffer, size_t buffer_size) {
+	std::vector<std::string> lines;
+	bool crlf = false;
+	
+	for (size_t i = 0, j = 0; i < buffer_size; i++) {
+		// Seek ahead to work out the line length
+		for (j = i; j < buffer_size; j++) {
+			// LF
+			if (buffer[j] == '\n') {
+				crlf = false;
+				break;
+			}
+			// CRLF
+			else if (buffer[j] == '\r' && buffer[j+1] == '\n') {
+				crlf = true;
+				break;
+			}
+			// null
+			else if (buffer[j] == '\0') {
+				crlf = false;
+				break;
+			}
+		}
+		std::string line((char *)&buffer[i], j - i);
+		
+		lines.push_back(line);
+		
+		if (crlf) i = j + 1;
+		else i = j;
+	}
+
+	return lines;
+}
+
+bool file_write_lines(std::vector<std::string> lines, const char *utf8_filename) {
+	return file_write_lines(lines, std::string(utf8_filename));
+}
+bool file_write_lines(std::vector<std::string> lines, const std::string &utf8_filename) {
+	return file_write_lines(lines, utf8_filename, false);
+}
+bool file_write_lines(std::vector<std::string> lines, const char *utf8_filename, bool append) {
+	return file_write_lines(lines, std::string(utf8_filename, append));
+}
+bool file_write_lines(std::vector<std::string> lines, const std::string &utf8_filename, bool append) {
+	nowide::ofstream file;
+
+	if (append)
+		file.open(utf8_filename, std::ios::app);
+	else
+		file.open(utf8_filename, std::ios::trunc);
+
+	if (!file.is_open()) {
+		std::string error = "Error opening \"" + utf8_filename + "\": " + strerror(errno);
+		app.ShowError(error);
+		return false;
+	}
+
+	for (auto &line : lines)
+		file << line << std::endl;
+
+	return true;
 }
--- src/platform.h
+++ src/platform.h
@@ -7,10 +7,21 @@
 #include "resource.h"
 
 #include <nowide/iostream.hpp>
+#include <string>
+#include <vector>
 
 // Loads an entire file in to memory.  The returned buffer must be later freed by the caller.
 char *file_as_buffer(size_t *buffer_size, const char *utf8_filename);
 char *file_as_buffer(size_t *buffer_size, const std::string &utf8_filename);
+
+// Convert a buffer into an array of strings (lines)
+std::vector<std::string> buffer_to_lines(char *buffer, size_t buffer_size);
+
+// Write each line in the array to the file
+bool file_write_lines(std::vector<std::string> lines, const char *utf8_filename);
+bool file_write_lines(std::vector<std::string> lines, const std::string &utf8_filename);
+bool file_write_lines(std::vector<std::string> lines, const char *utf8_filename, bool append);
+bool file_write_lines(std::vector<std::string> lines, const std::string &utf8_filename, bool append);
 
 // Shows a file dialog (should hang the current thread) and returns the utf8 filename picked by the
 // user or nullptr.  If non-null is returned, it must be later freed by the caller.
